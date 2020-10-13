5b5d2d4d95185cd28a92234818e904991c0bf6aa
jithinlal <jithinlal@softwareassociates.in>
2020-10-13 18:15:19 +0530

    Ionic commit

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBGFFY7fb9Fon6QXGdaoskNAncpt0NcTeM',
+	authDomain: 'hunt-b7c76.firebaseapp.com',
+	databaseURL: 'https://hunt-b7c76.firebaseio.com',
+	projectId: 'hunt-b7c76',
+	storageBucket: 'hunt-b7c76.appspot.com',
+	messagingSenderId: '628392656319',
+	appId: '1:628392656319:web:8801df3a004ebdd413b50a',
+};
+
+export default firebaseConfig;
